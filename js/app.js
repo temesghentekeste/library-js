16fd3d85166e6a8520e57a821f274fb3d98fb95f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-29 21:09:10 +0300

    Add delete book functionality; add animation upon delete

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@ const UITextTitle = document.querySelector('.txt-title');
 const UINumPages = document.querySelector('.num-pages');
 const UIChKStatus = document.querySelector('.chk-status');
 const UITableBody = document.querySelector(".table-body");
-const btnAddBook = document.querySelector(".btn-add-book");
-const btnshowForm = document.querySelector(".btn-show-form");
+const UIBtnAddBook = document.querySelector(".btn-add-book");
+const UIBtnToggleForm = document.querySelector(".btn-show-form");
 const UIFormContainer = document.querySelector(".form-popup");
 
-btnAddBook.addEventListener('click', addBookToLibrary);
+UIBtnAddBook.addEventListener('click', addBookToLibrary);
 UIChKStatus.addEventListener("change", getStatus);
-
+UITableBody.addEventListener('click', deleteEdit);
 
 function Book(author, title, pages, status) {
   this.author = author;
@@ -31,6 +31,7 @@ function addBookToLibrary(e) {
   const newBook = new Book(author, title, pages, status);
 
   const bookRow = document.createElement("tr");
+  bookRow.classList.add("book");
 
   // New book item
 
@@ -48,24 +49,26 @@ function addBookToLibrary(e) {
   const newBookPages = document.createElement("td");
   newBookPages.innerText = newBook.pages;
   bookRow.appendChild(newBookPages);
-  
+
   // Status column
   const newBookStatus = document.createElement("td");
   newBookStatus.innerHTML = `${newBook.status ? "Read" : "Not Read"}`;
   bookRow.appendChild(newBookStatus);
-  
-  // Delete Button
+
+  // Actions column
+  const newBookActions = document.createElement("td");
   const deleteBook = document.createElement("button");
   deleteBook.innerHTML = '<i class="fas fa-trash"></i>';
   deleteBook.classList.add("trash-btn");
-  bookRow.appendChild(deleteBook);
+  newBookActions.appendChild(deleteBook);
+  bookRow.appendChild(newBookActions);
 
   // Append the row back to tbody
   UITableBody.appendChild(bookRow);
 }
 
 
-btnshowForm.addEventListener('click', e => {
+UIBtnToggleForm.addEventListener('click', e => {
   UIFormContainer.classList.toggle('hidden');
 })
 
@@ -79,6 +82,24 @@ btnshowForm.addEventListener('click', e => {
    }
 
    return bookStatus;
+ }
+
+ // Function to delete or edit
+ function deleteEdit(e) {
+    const selectedItem = e.target;
+    const bookRow = selectedItem.parentElement.parentElement;
+    
+    if (selectedItem.classList.contains("trash-btn")) {
+      console.log("Trash btn");
+      console.log(bookRow);
+      bookRow.classList.add("fall");
+      bookRow.addEventListener("transitionend", function () {
+        bookRow.remove();
+      });
+    } else {
+      console.log("Not trash btn");
+      return;
+    }
  }
 
 
