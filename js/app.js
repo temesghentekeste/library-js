c5e62a3acd7c956a5fa99ac8caec7138df5975c4
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-30 03:56:44 +0300

    Add update book feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 let localStorageItem = "myBooksLibrary";
 let bookStatus = false;
+let isBookToUpdate = false;
+let currentBook;
+let tableRowsSelector =  '#books tbody';
+
+
 const UITextAuthour = document.querySelector(".txt-author");
 const UITextTitle = document.querySelector(".txt-title");
 const UINumPages = document.querySelector(".num-pages");
@@ -22,13 +27,17 @@ function Book(author, title, pages, status) {
 
 function addBookToLibrary(e) {
   e.preventDefault();
-  console.log(UIChKStatus, UIChKStatus.value);
 
   const author = UITextAuthour.value.trim();
   const title = UITextTitle.value.trim();
   const pages = UINumPages.value;
   const status = bookStatus;
   const newBook = new Book(author, title, pages, status);
+
+  if( isBookToUpdate === true) {
+    updateUI(currentBook);
+    return;
+  }
 
   const bookRow = document.createElement("tr");
   bookRow.classList.add("book");
@@ -37,15 +46,17 @@ function addBookToLibrary(e) {
 
   // Add ID to bookRow: newBook
   const books = getBooks();
+  console.log(books, "call getbooks");
   console.log(books);
   let id;
   if (books.length > 0) {
-    id = books[books.length - 1].id + 1;
+    id = books[books.length - 1].id
+    id = id + 1;
   } else {
     id = 1;
   }
   newBook.id = id;
-  bookRow.setAttribute("id", id);
+  bookRow.setAttribute("id", `item-${id}`);
   // Author column
   const newBookAuthor = document.createElement("td");
   newBookAuthor.innerText = newBook.author;
@@ -72,16 +83,23 @@ function addBookToLibrary(e) {
   deleteBook.innerHTML = '<i class="fas fa-trash"></i>';
   deleteBook.classList.add("trash-btn");
   newBookActions.appendChild(deleteBook);
+
+  const editBook = document.createElement("button");
+  editBook.innerHTML = '<i class="fas fa-edit"></i>';
+  editBook.classList.add("edit-btn");
+  newBookActions.appendChild(editBook);
   bookRow.appendChild(newBookActions);
 
   // Save book to LS
   saveBookLS(newBook);
   // Append the row back to tbody
   UITableBody.appendChild(bookRow);
+  console.log(bookRow, "saveBookLS");
 }
 
 UIBtnToggleForm.addEventListener("click", (e) => {
   UIFormContainer.classList.toggle("hidden");
+  return;
 });
 
 function getStatus(event) {
@@ -89,7 +107,6 @@ function getStatus(event) {
     bookStatus = true;
   } else {
     bookStatus = false;
-    console.log(event.target.value, bookStatus);
   }
 
   return bookStatus;
@@ -99,16 +116,18 @@ function getStatus(event) {
 function deleteEdit(e) {
   const selectedItem = e.target;
   const bookRow = selectedItem.parentElement.parentElement;
+  currentBook = getBook(bookRow);
 
   if (selectedItem.classList.contains("trash-btn")) {
     bookRow.classList.add("fall");
     bookRow.addEventListener("transitionend", function () {
       bookRow.remove();
+      resetForm();
     });
-    const bookToDelte = getBook(bookRow);
-    deleteBookLS(bookToDelte);
+    deleteBookLS(currentBook);
+  } else if (selectedItem.classList.contains("edit-btn")) {
+    displayCurrentBook(currentBook);
   } else {
-    console.log("Not trash btn");
     return;
   }
 }
@@ -122,6 +141,8 @@ const getBooks = () =>
 // Save book to LS
 function saveBookLS(book) {
   let myBooksLibrary;
+  // let id = parseId(book.id);
+  // book.id = id;
   if (localStorage.getItem(localStorageItem)) {
     myBooksLibrary = JSON.parse(localStorage.getItem(localStorageItem));
   } else {
@@ -135,7 +156,6 @@ function saveBookLS(book) {
 // Load books from LS
 // Load Todos
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded");
   const books = getBooks();
   books.forEach((book) => {
     // Render books
@@ -147,12 +167,14 @@ document.addEventListener("DOMContentLoaded", () => {
 const render = (book) => {
   // Create and append a task to the taskList
   const html = `
-    <tr class="book" id="${book.id}">
+    <tr class="book" id="item-${book.id}">
       <td>${book.author}</td>
       <td>${book.title}</td>
       <td>${book.pages}</td>
       <td>${book.status ? "Read" : "Not Read"}</td>
-      <td><button class="trash-btn"><i class="fas fa-trash"></i></button> </td>
+      <td><button class="trash-btn"><i class="fas fa-trash"></i></button> 
+      <button class="edit-btn"><i class="fas fa-edit"></i></button>
+      </td>
     </tr>
   `;
 
@@ -160,18 +182,84 @@ const render = (book) => {
 };
 
 // Get Book
-const getBook = selectedBook => {
+const getBook = (selectedBook) => {
   let id = selectedBook.getAttribute("id");
-  id = parseInt(id);
+  if (id === null) {
+    return;
+  }
+  id = parseId(id);
   const books = getBooks();
-  const book = books.find(b => b.id == id);
+  const book = books.find((b) => b.id == id);
   return book;
-}
+};
 
 // Delete Book LS
-const deleteBookLS = book => {
+const deleteBookLS = (book) => {
   let books = getBooks();
-  books = books.filter( b => b.id != book.id);
+  books = books.filter((b) => b.id != book.id);
   localStorage.setItem(localStorageItem, JSON.stringify(books));
+};
 
+// Display Current Book
+const displayCurrentBook = (book) => {
+  console.log("Book", book);
+  isBookToUpdate = true;
+  UIBtnAddBook.textContent = "Update Book"
+  if (UIFormContainer.classList.contains("hidden")) {
+    UIFormContainer.classList.remove("hidden");
+  }
+  UITextAuthour.value = book.author;
+  UITextTitle.value = book.title;
+  UINumPages.value = book.pages;
+  UIChKStatus.checked = book.status;
+};
+
+// Update Current Book
+const updateBook = currentBook => {
+}
+
+// Update UI
+const updateUI = currentBook => {
+  let id = currentBook.id;
+  let book= document.querySelector(`${tableRowsSelector} #item-${id}`);
+  const html = getHTML(currentBook);
+  console.log(html);
+  book.innerHTML = html;
+  resetForm();
+}
+
+const resetForm = () => {
+  UITextAuthour.value = "";
+  UITextTitle.value = "book.title";
+  UINumPages.value = 0;
+  UIChKStatus.checked = false;
+
+  isBookToUpdate = false;
+  UIBtnAddBook.textContent = "Add New Book";
+}
+
+const parseId = id  => {
+  const arrId = id.split("-");
+  return parseInt(arrId[1]);
+}
+
+// Get HTML
+const getHTML = () => {
+  const author = UITextAuthour.value.trim();
+  const title = UITextTitle.value.trim();
+  const pages = UINumPages.value;
+  const status = bookStatus;
+  const newBook = new Book(author, title, pages, status);
+  const html = `
+    <tr class="book" id="item-${currentBook.id}">
+      <td>${UITextAuthour.value.trim()}</td>
+      <td>${UITextTitle.value.trim()}</td>
+      <td>${UINumPages.value}</td>
+      <td>${bookStatus ? "Read" : "Not Read"}</td>
+      <td><button class="trash-btn"><i class="fas fa-trash"></i></button> 
+      <button class="edit-btn"><i class="fas fa-edit"></i></button>
+      </td>
+    </tr>
+  `;
+  return html;
 }
